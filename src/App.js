0551d1f98c8024b19a022e1247b90c245117cea5
sajidshahi <sajid.mohammed@outlook.com>
2024-03-27 18:12:29 -0400

    Enhancement made

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar'
+import News from './Components/News'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar/>
+        <News/>
+       
       </div>
     )
   }
 }
-
