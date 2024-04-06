68d1e5857dd1dc9f7f5760ccb0483dfea4043b77
sajidshahi <sajid.mohammed@outlook.com>
2024-04-06 14:39:21 -0400

    Enhancemnt git st q cd.. clear enhancement Enhancemnt

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,22 +2,32 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, ImageUrl, NewsUrl } = this.props;
+    let { title, description, ImageUrl, NewsUrl, author, date, source } = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}>
+                {source}
+              </span>
           <img src={ImageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title"> {title}</h5>
+            <h5 className="card-title">
+              {" "}
+              {title}
+            </h5>
             <p className="card-text"> {description}</p>
-
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {author} on {date}
+              </small>
+            </p>
             <a
-            rel= "noreferrer"
+              rel="noreferrer"
               href={NewsUrl}
               target="_blank"
               className="btn btn-dark btn-sm"
             >
-              Read more..
+              Read more..{" "}
             </a>
           </div>
         </div>
